96e1867e4fe70066104e7c7e7fae1b19ea6c7edd
Henri Cattoire <henricattoire@yahoo.com>
2023-04-04 16:04:57 +0200

    add generate and mutation.

--- erpy/framework/graph.py
+++ erpy/framework/graph.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
 import abc
+import copy
 from typing import List, TYPE_CHECKING
 
+from erpy import random_state
+from erpy.framework.parameters import FixedParameter, SynchronizedParameter, Parameter, ContinuousParameter
 from erpy.framework.specification import Specification
 
 if TYPE_CHECKING:
@@ -18,8 +21,18 @@ def unfold_list(nodes: List[DirectedNode]) -> [Specification]:
 
 
 class DirectedGraph(metaclass=abc.ABCMeta):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, root: DirectedNode) -> None:
+        self.root = root
+
+    def generate(self) -> DirectedGraph:
+        graph = copy.deepcopy(self)
+        graph.root.generate()
+        return graph
+
+    def mutate(self) -> DirectedGraph:
+        graph = copy.deepcopy(self)
+        graph.root.mutate()
+        return graph
 
     @abc.abstractmethod
     def unfold(self) -> Specification:
@@ -27,9 +40,34 @@ class DirectedGraph(metaclass=abc.ABCMeta):
 
 
 class DirectedNode(metaclass=abc.ABCMeta):
-    def __init__(self, repeated: int = 0) -> None:
+    def __init__(self, children: List[DirectedNode] = [], repeated: int = 0) -> None:
+        self.children = children
         # number of times a node points to itself
-        self.repeated: int = repeated
+        self.repeated = repeated
+
+    @property
+    def mutable_parameters(self):
+        pars: List[Parameter] = []
+        for name in vars(self):
+            var = self.__getattribute__(name)
+            if isinstance(var, Parameter) and not (
+                    isinstance(var, FixedParameter) or isinstance(var, SynchronizedParameter)):
+                pars.append(var)
+        return pars
+
+    def generate(self):
+        for par in self.mutable_parameters:
+            par.set_random_value()
+        for child in self.children:
+            child.generate()
+
+    def mutate(self):
+        for par in self.mutable_parameters:
+            if isinstance(par, ContinuousParameter):
+                value_range = par.high - par.low
+
+                noise = value_range * random_state.normal(scale=0.01)
+                par.value += noise
 
     @abc.abstractmethod
     def unfold(self) -> Specification:
