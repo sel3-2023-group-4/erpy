ef86c2b177cf70d3d7fa3def07c2d4b471d4ab8f
Wouter01 <wouterhennen@gmail.com>
2023-05-05 19:49:11 +0200

    Add DiscreteActions wrapper
    Signed-off-by: Wouter01 <wouterhennen@gmail.com>

--- erpy/interfaces/mujoco/viewer.py
+++ erpy/interfaces/mujoco/viewer.py
@@ -4,9 +4,10 @@ import numpy as np
 from dm_control import viewer
 from dm_env import TimeStep
 
-from erpy.interfaces.mujoco.environment import MJCEnvironmentConfig, dm_control_to_gym_environment
+from erpy.interfaces.mujoco.environment import MJCEnvironmentConfig, dm_control_to_gym_environment, DiscreteActions
 from erpy.interfaces.mujoco.gym_wrapper import get_clean_obs, vectorize_observations
 from erpy.interfaces.mujoco.phenome import MJCRobot
+from stable_baselines_integration.robot.controller import PPOMJCEnvironmentConfig
 
 
 def evaluate_with_dm_control_viewer(env_config: MJCEnvironmentConfig, robot: MJCRobot) -> None:
@@ -14,7 +15,11 @@ def evaluate_with_dm_control_viewer(env_config: MJCEnvironmentConfig, robot: MJC
                                     wrap2gym=False)
 
     gym_env = dm_control_to_gym_environment(config=env_config, environment=dm_env)
-    robot.controller.set_environment(gym_env)
+    if isinstance(env_config, PPOMJCEnvironmentConfig):
+        env = DiscreteActions(gym_env, robot.morphology.actions)
+        robot.controller.set_environment(env)
+    else:
+        robot.controller.set_environment(gym_env)
 
     def policy_fn(timestep: TimeStep) -> np.ndarray:
         observations = get_clean_obs(timestep)
