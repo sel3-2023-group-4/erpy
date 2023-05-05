e581182692086e370fb9ed0b487c2948ee1a2ac3
Wouter01 <wouterhennen@gmail.com>
2023-05-05 21:45:01 +0200

    ppo fix
    Signed-off-by: Wouter01 <wouterhennen@gmail.com>

--- erpy/interfaces/mujoco/viewer.py
+++ erpy/interfaces/mujoco/viewer.py
@@ -15,11 +15,11 @@ def evaluate_with_dm_control_viewer(env_config: MJCEnvironmentConfig, robot: MJC
                                     wrap2gym=False)
 
     gym_env = dm_control_to_gym_environment(config=env_config, environment=dm_env)
-    if isinstance(env_config, PPOMJCEnvironmentConfig):
-        env = DiscreteActions(gym_env, robot.morphology.actions)
-        robot.controller.set_environment(env)
-    else:
-        robot.controller.set_environment(gym_env)
+
+    # Add this for PPO
+    # gym_env = DiscreteActions(gym_env, robot.morphology.actions)
+
+    robot.controller.set_environment(gym_env)
 
     def policy_fn(timestep: TimeStep) -> np.ndarray:
         observations = get_clean_obs(timestep)
