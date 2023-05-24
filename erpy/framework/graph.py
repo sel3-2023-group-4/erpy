b3568458a793f2935ce1618bfd18333e659c725b
Henri Cattoire <henricattoire@yahoo.com>
2023-05-24 22:39:02 +0200

    add comments.

--- erpy/framework/graph.py
+++ erpy/framework/graph.py
@@ -5,8 +5,7 @@ import copy
 from typing import List, TYPE_CHECKING
 
 from erpy import random_state
-from erpy.framework.parameters import FixedParameter, SynchronizedParameter, Parameter, ContinuousParameter, \
-    DiscreteParameter
+from erpy.framework.parameters import FixedParameter, SynchronizedParameter, Parameter, ContinuousParameter
 from erpy.framework.specification import Specification
 
 if TYPE_CHECKING:
@@ -14,6 +13,17 @@ if TYPE_CHECKING:
 
 
 def unfold_list(nodes: List[DirectedNode]) -> [Specification]:
+    """
+    This function unfolds a list of directed nodes to a list of corresponding specifications.
+
+    The remarkable part of this function is that the unfolded node (read specification) will
+    appear as many times in the list of specifications as the original node was repeated (+ 1).
+    For example, a node that is points 2 times to itself (is 2 times repeated), will appear
+    as a specification 3 times.
+
+    :param nodes: the list of nodes to be unfolded.
+    :return: the list of unfolded nodes (read: specifications).
+    """
     specifications = []
     for node in nodes:
         specification = node.unfold()
@@ -22,15 +32,34 @@ def unfold_list(nodes: List[DirectedNode]) -> [Specification]:
 
 
 class DirectedGraph(metaclass=abc.ABCMeta):
+    """
+    A directed graph used for indirect genetic encodings.
+
+    :var root: the root of the graph (this root should link to the other nodes
+               of the graph.
+    """
     def __init__(self, root: DirectedNode) -> None:
         self.root = root
 
     def generate(self) -> DirectedGraph:
+        """
+        Generate a new directed graph from this directed graph; keeping the structure
+        but with random values for the nodes. Useful in evolutionary algorithms.
+
+        :return: A new directed graph generated from this one.
+        """
         graph = copy.deepcopy(self)
         graph.root.generate()
         return graph
 
     def mutate(self) -> DirectedGraph:
+        """
+        Generate a new directed graph from this directed graph; keeping the structure
+        but with slightly mutated values for the nodes. Useful in evolutionary
+        algorithms.
+
+        :return: A new mutated graph generated from this one.
+        """
         graph = copy.deepcopy(self)
         graph.root.mutate()
         return graph
@@ -38,6 +67,10 @@ class DirectedGraph(metaclass=abc.ABCMeta):
     def get_labels_and_values(self, node: DirectedNode, idx: int = 0):
         """
         Get labels and parameters for this node and all its children.
+
+        :param node: the node to get labels and parameters for.
+        :param idx: idx used to distinguish children of the same class.
+        :return: a list of tuples with the name of the parameters and their values.
         """
         pars = []
         for name in vars(node):
@@ -57,17 +90,33 @@ class DirectedGraph(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def unfold(self) -> Specification:
+        """
+        Unfold this graph to a specification used in the simulation environment.
+
+        :return: A spec.
+        """
         raise NotImplementedError
 
 
 class DirectedNode(metaclass=abc.ABCMeta):
+    """
+    A directed node used in DirectedGraph.
+
+    :var children: a list of children nodes directly linked to this node (default = []).
+    :var repeated: the amount of times this node links to itself (default = 0).
+    """
     def __init__(self, children: List[DirectedNode] = [], repeated: int = 0) -> None:
+        assert repeated >= 0
         self.children = children
         # number of times a node points to itself
         self.repeated = repeated
 
     @property
     def mutable_parameters(self):
+        """
+        A list of mutable parameters this node has. A mutable parameter is an erpy Parameter which is not
+        Fixed or Synchronized.
+        """
         pars: List[Parameter] = []
         for name in vars(self):
             var = self.__getattribute__(name)
@@ -77,20 +126,39 @@ class DirectedNode(metaclass=abc.ABCMeta):
         return pars
 
     def is_recursive(self) -> bool:
+        """
+        A recursive node is a node that points to itself; i.e. is not repeated.
+        """
         return self.repeated > 0
 
     def generate(self):
+        """
+        Set random values for the mutable parameters and recursively do this for all the children.
+
+        NOTE: this change happens in place.
+        """
         for par in self.mutable_parameters:
             par.set_random_value()
         for child in self.children:
             child.generate()
 
     def mutate(self):
+        """
+        Mutate the values for the mutable parameters and recursively do this for all the children.
+
+        NOTE: this change happens in place.
+        """
         self.mutate_mutable_parameters()
         for child in self.children:
             child.mutate()
 
     def mutate_mutable_parameters(self):
+        """
+        Actually mutate the mutable parameters.
+
+        erpy Parameters that get mutated.
+        - ContinuousParemeters
+        """
         for par in self.mutable_parameters:
             if isinstance(par, ContinuousParameter):
                 value_range = par.high - par.low
@@ -100,4 +168,9 @@ class DirectedNode(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def unfold(self) -> Specification:
+        """
+        Unfold this node to a specification used in the simulation environment.
+
+        :return: A spec.
+        """
         raise NotImplementedError
